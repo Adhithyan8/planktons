454eabc0629af03fe2d6151dba8abf54d15a8deb
Adhithyan8 <k.adhithyan@gmail.com>
2024-01-03 05:51:54 +0000

    Minor refactoring

--- pretrained.py
+++ pretrained.py
@@ -2,8 +2,15 @@ import torch
 from datasets import load_dataset
 from torch.utils.data import DataLoader
 from torchvision import transforms as T
-from transformers import AutoImageProcessor, ResNetModel
-
+from transformers import (
+    AutoImageProcessor,
+    AutoProcessor,
+    BeitModel,
+    ResNetModel,
+    ViTModel,
+    Dinov2Model,
+    CLIPVisionModelWithProjection,
+)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -11,38 +18,60 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 train_dataset = load_dataset(
     "/local_storage/users/adhkal/planktons_dataset", "2013-14", split="train"
 )
-# torch format
 train_dataset = train_dataset.with_format("torch")
 # convert to 3 channels
 train_dataset = train_dataset.map(
     lambda x: {"image": x["image"].repeat(3, 1, 1), "label": x["label"]},
 )
 
-# load pretrained model
-feature_extractor = AutoImageProcessor.from_pretrained("microsoft/resnet-152")
-# preprocess
+# load pretrained model (pick one)
+# feature_extractor = AutoImageProcessor.from_pretrained("microsoft/resnet-152")
+# feature_extractor = AutoImageProcessor.from_pretrained(
+#     "google/vit-base-patch16-224-in21k"
+# )
+# feature_extractor = AutoImageProcessor.from_pretrained(
+#     "microsoft/beit-base-patch16-224-pt22k"
+# )
+# feature_extractor = AutoImageProcessor.from_pretrained("facebook/dinov2-base")
+feature_extractor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+# preprocess (CLIP is different)
+# train_dataset = train_dataset.map(
+#     lambda x: {"image": feature_extractor(x["image"]), "label": x["label"]},
+# )
 train_dataset = train_dataset.map(
-    lambda x: {"image": feature_extractor(x["image"]), "label": x["label"]},
-)
+    lambda x: {"image": feature_extractor(images=x["image"]), "label": x["label"]},
+)  # CLIPVisionModelWithProjection
 
 # dataloader
-train_loader = DataLoader(train_dataset, batch_size=8, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=False)
 
 # save the output vectors
-vecs = torch.empty((0, 2048))
-tags = torch.empty((0,))
+# vecs = torch.empty((0, 2048)) # ResNetModel
+vecs = torch.empty((0, 512))  # CLIPVisionModelWithProjection
+# vecs = torch.empty((0, 768)) # other models
+# tags = torch.empty((0,))
 
-# model
-model = ResNetModel.from_pretrained("microsoft/resnet-152")
+# model (pick one)
+# model = ResNetModel.from_pretrained("microsoft/resnet-152")
+# model = ViTModel.from_pretrained("google/vit-base-patch16-224-in21k")
+# model = BeitModel.from_pretrained("microsoft/beit-base-patch16-224-pt22k")
+# model = Dinov2Model.from_pretrained("facebook/dinov2-base")
+model = CLIPVisionModelWithProjection.from_pretrained("openai/clip-vit-base-patch32")
+
 model.to(device).eval()
-
 for batch in train_loader:
     images = batch["image"]["pixel_values"].squeeze(1)
     tags = torch.cat((tags, batch["label"]), dim=0)
     with torch.no_grad():
-        outputs = model(images.to(device)).pooler_output.squeeze()  # (B, 2048)
+        # outputs = model(images.to(device)).pooler_output.squeeze()  # (B, 2048)
+        outputs = model(images.to(device)).image_embeds.squeeze()  # (B, 768) CLIP
         vecs = torch.cat((vecs, outputs.cpu()), dim=0)
 
 # save the vectors
-torch.save(vecs, "tensors/resnet_vecs.pt")
-torch.save(tags, "tensors/tags.pt")
+torch.save(tags, "embeddings/tags.pt")
+# torch.save(vecs, "embeddings/resnet_vecs.pt")
+# torch.save(vecs, "embeddings/vit_vecs.pt")
+# torch.save(vecs, "embeddings/beit_vecs.pt")
+# torch.save(vecs, "embeddings/dinov2_vecs.pt")
+torch.save(vecs, "embeddings/clip_vecs.pt")
--- tsne.py
+++ tsne.py
@@ -4,8 +4,12 @@ import matplotlib.pyplot as plt
 import openTSNE
 
 # load the vectors
-vecs = torch.load("tensors/resnet_vecs.pt").numpy()  # (N, 2048)
-tags = torch.load("tensors/tags.pt").numpy()  # (N,)
+# # vecs = torch.load("embeddings/resnet_vecs.pt").numpy()  # (N, 2048)
+# # vecs = torch.load("embeddings/vit_vecs.pt").numpy()  # (N, 768)
+# # vecs = torch.load("embeddings/beit_vecs.pt").numpy()  # (N, 768)
+# # vecs = torch.load("embeddings/dinov2_vecs.pt").numpy()  # (N, 768)
+vecs = torch.load("embeddings/clip_vecs.pt").numpy()  # (N, 512)
+tags = torch.load("embeddings/tags.pt").numpy()  # (N,)
 
 # perform tsne with multiscale embedding
 affinities_multiscale_mixture = openTSNE.affinity.Multiscale(
@@ -21,11 +25,12 @@ embedding = openTSNE.TSNE(n_jobs=8).fit(
     initialization=init,
 )
 
-# save the embedding
-np.save("embedding.npy", embedding)
-
-# # load the embedding
-# embedding = np.load("embedding.npy")
+# save the tsne embedding
+# np.save("embeddings/resnet_tsne.npy", embedding)
+# np.save("embeddings/vit_tsne.npy", embedding)
+# np.save("embeddings/beit_tsne.npy", embedding)
+# np.save("embeddings/dinov2_tsne.npy", embedding)
+np.save("embeddings/clip_tsne.npy", embedding)
 
 # plot the embedding
 plt.figure(figsize=(10, 10))
@@ -37,5 +42,5 @@ plt.scatter(
     s=0.1,
 )
 plt.axis("off")
-plt.savefig("embedding.png", dpi=300)
+plt.savefig("figures/clip_tsne.png", dpi=300)
 plt.close()
