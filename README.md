69eaaa0f4a3ee5727c9ab0bc51bab8d185192b24
Adhithyan Kalaivanan <k.adhithyan@gmail.com>
2024-01-13 11:57:41 +0100

    Visualization added

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Discovery of novel plankton species from visual images
 
-### Dataset: WHOI-Plankton
+### Dataset:
+- [WHOI Plankton](https://darchive.mblwhoilibrary.org/collections/aad045e7-1fcf-5650-82ee-c62bd604d225)
+
+### Other resources:
+- [Plankton net](https://planktonnet.awi.de/)
+- [IFCB](https://ifcb-data.whoi.edu/dashboard)
--- explore.py
+++ explore.py
@@ -11,96 +11,87 @@ from PIL import Image
 train_dataset = load_dataset(
     "planktons_dataset", "2013-14", split="train", trust_remote_code=True
 )
-print(train_dataset.features["label"].names)
-# # embeddings
-# embeddings = np.load("embeddings/tsne_clip.npy")
+# embeddings
+embeddings = np.load("embeddings/tsne_clip.npy")
+# labels
+names = train_dataset.features["label"].names
+indices = np.load("embeddings/tags.npy").astype(int)
+labels = [names[idx] for idx in indices]
 
-# labelmap = {}
-# # label mapping
-# with open("labels.txt", "r") as f:
-#     for line in f:
-#         idx, label = line.strip().split(":")
-#         labelmap[int(idx)] = label
+# set colors
+colormap = [f"hsl({h},50%, 50%)" for h in np.linspace(0, 360, max(indices) + 1)]
+colors = np.array([colormap[idx] for idx in indices])
 
-# # labels
-# indices = np.load("embeddings/tags.npy").astype(int)
-# labels = [labelmap[idx] for idx in indices]
+fig = go.Figure()
+for i in range(max(indices) + 1):
+    fig.add_trace(
+        go.Scattergl(
+            x=embeddings[indices == i, 0],
+            y=embeddings[indices == i, 1],
+            mode="markers",
+            marker=dict(size=5, opacity=0.8, color=colors[indices == i]),
+            name=names[i],
+            # set id as embedding index
+            ids=[str(idx) for idx in np.where(indices == i)[0]],
+        )
+    )
 
-# # set colors
-# colormap = [f"hsl({h},50%, 50%)" for h in np.linspace(0, 360, max(indices) + 1)]
-# colors = np.array([colormap[idx] for idx in indices])
+fig.update_traces(hoverinfo="none", hovertemplate=None)
+fig.update_layout(
+    xaxis=dict(visible=False),
+    yaxis=dict(visible=False),
+    autosize=False,
+    height=700,
+    width=1500,
+)
 
-# fig = go.Figure()
-# for i in range(max(indices) + 1):
-#     fig.add_trace(
-#         go.Scattergl(
-#             x=embeddings[indices == i, 0],
-#             y=embeddings[indices == i, 1],
-#             mode="markers",
-#             marker=dict(size=5, opacity=0.5, color=colors[indices == i]),
-#             name=labelmap[i],
-#             # set id as embedding index
-#             ids=[str(idx) for idx in np.where(indices == i)[0]],
-#         )
-#     )
-
-
-# fig.update_traces(hoverinfo="none", hovertemplate=None)
-# fig.update_layout(
-#     xaxis=dict(visible=False),
-#     yaxis=dict(visible=False),
-#     autosize=False,
-#     height=800,
-#     width=800,
-# )
-
-# app = Dash(__name__)
-# app.layout = html.Div(
-#     className="container",
-#     children=[
-#         dcc.Graph(
-#             id="graph",
-#             figure=fig,
-#             clear_on_unhover=True,
-#         ),
-#         dcc.Tooltip(id="tooltip"),
-#     ],
-# )
+app = Dash(__name__)
+app.layout = html.Div(
+    className="container",
+    children=[
+        dcc.Graph(
+            id="graph",
+            figure=fig,
+            clear_on_unhover=True,
+        ),
+        dcc.Tooltip(id="tooltip"),
+    ],
+)
 
 
-# @callback(
-#     Output("tooltip", "show"),
-#     Output("tooltip", "bbox"),
-#     Output("tooltip", "children"),
-#     Input("graph", "hoverData"),
-# )
-# def display_hover_data(hoverData):
-#     if hoverData is None:
-#         return False, no_update, no_update
+@callback(
+    Output("tooltip", "show"),
+    Output("tooltip", "bbox"),
+    Output("tooltip", "children"),
+    Input("graph", "hoverData"),
+)
+def display_hover_data(hoverData):
+    if hoverData is None:
+        return False, no_update, no_update
 
-#     hover_data = hoverData["points"][0]
-#     bbox = hover_data["bbox"]
-#     im_path = train_dataset[int(hover_data["id"])]["image"].filename
-#     im = Image.open(im_path)
+    hover_data = hoverData["points"][0]
+    bbox = hover_data["bbox"]
+    im_path = train_dataset[int(hover_data["id"])]["image"].filename
+    im = Image.open(im_path)
 
-#     # base64 image
-#     buffer = io.BytesIO()
-#     im.save(buffer, format="PNG")
-#     encoded_image = base64.b64encode(buffer.getvalue()).decode("utf-8")
-#     im_url = "data:image/png;base64,{}".format(encoded_image)
+    # base64 image
+    buffer = io.BytesIO()
+    im.save(buffer, format="PNG")
+    encoded_image = base64.b64encode(buffer.getvalue()).decode("utf-8")
+    im_url = "data:image/png;base64,{}".format(encoded_image)
 
-#     children = [
-#         html.Div(
-#             [
-#                 html.Img(src=im_url, style={"width": "200px"}),
-#                 html.P(
-#                     f"{indices[int(hover_data['id'])]} - {labels[int(hover_data['id'])]}"
-#                 ),
-#             ]
-#         )
-#     ]
-#     return True, bbox, children
+    children = [
+        html.Div(
+            [
+                html.Img(src=im_url, style={"width": "200px"}),
+                html.P(
+                    f"{indices[int(hover_data['id'])]} - {labels[int(hover_data['id'])]}"
+                ),
+            ]
+        )
+    ]
+    return True, bbox, children
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
