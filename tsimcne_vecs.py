9c3dcc2b2eef6094c8c57e92b511a60514edc238
Adhithyan8 <k.adhithyan@gmail.com>
2024-02-14 14:15:46 +0000

    Life is suffering

--- tsimcne_vecs.py
+++ tsimcne_vecs.py
@@ -1,12 +1,13 @@
+import time
+
 import numpy as np
 import pandas as pd
-from PIL import Image
 import torch
+from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms as T
-from torchvision.io import read_image
-from tsimcne.tsimcne import TSimCNE
 from transformers import AutoImageProcessor, Dinov2Model
+from tsimcne.tsimcne import TSimCNE
 
 processor = AutoImageProcessor.from_pretrained("facebook/dinov2-base")
 
@@ -23,8 +24,10 @@ class PlanktonsPT(Dataset):
         img_path = self.img_labels.iloc[idx, 0]
         with open(img_path, "rb") as f:
             image = Image.open(f)
-            image = T.Resize((224, 224))(image)  # to speed up training
+            image = T.Resize((224, 224))(image)
             image = processor(image)["pixel_values"][0]
+            image = image.transpose(1, 2, 0)
+            image = T.ToPILImage(mode="RGB")(image)
         label = self.img_labels.iloc[idx, 1]
         return image, label
 
@@ -34,20 +37,49 @@ planktons_dataset = PlanktonsPT(
     annotations_file="/local_storage/users/adhkal/planktons_pytorch/annotations.csv"
 )
 
-backbone = Dinov2Model.from_pretrained("facebook/dinov2-base")
-# mlp with one hidden layer as the projection head
-projection_head = torch.nn.Sequential(
-    torch.nn.Linear(768, 1024),
-    torch.nn.ReLU(),
-    torch.nn.Linear(1024, 128),
-)
 
-# tsimcne object (using their default rand initialized resnet18 as backbone)
+# edit the forward method to return the pooler output
+class Dinov2ModelModified(Dinov2Model):
+    def forward(self, x):
+        output = super().forward(x)
+        return output.pooler_output.squeeze()
+
+
+backbone = Dinov2ModelModified.from_pretrained("facebook/dinov2-base")
+# freeze the backbone
+for name, param in backbone.named_parameters():
+    param.requires_grad = False
+
+
+# mlp with one hidden layer as the projection head
+# need layers as attributes of the model for the tsimcne object
+class FC(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(FC, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+
+        self.layers = torch.nn.Sequential(
+            torch.nn.Linear(input_dim, hidden_dim),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Linear(hidden_dim, output_dim),
+        )
+        self.flatten = torch.nn.Flatten()
+
+    def forward(self, x):
+        x = self.flatten(x)
+        return self.layers(x)
+
+
+projection_head = FC(768, 1024, 128)
+
+# tsimcne object (try different augmentation strategies)
 tsimcne = TSimCNE(
     backbone=backbone,
     projection_head=projection_head,
-    total_epochs=[1, 1, 1],
-    batch_size=2,
+    total_epochs=[500, 50, 250],  # resnet was run with [500, 50, 250] and 32x32 images
+    batch_size=256,  # cannot increase more than 256
 )
 
 # train
@@ -57,4 +89,4 @@ tsimcne.fit(planktons_dataset)
 vecs = tsimcne.transform(planktons_dataset)
 
 # save the output vectors
-np.save("embeddings/vec_tsimcne_resnet18.npy", vecs)
+np.save("embeddings/vec_tsimcne_dinov2.npy", vecs)
