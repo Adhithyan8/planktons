687524f45c93628fbe4767ea7dda94ebffd0e385
Adhithyan Kalaivanan <k.adhithyan@gmail.com>
2024-02-27 16:42:17 +0100

    c'est la vie

--- contrastive_learning.py
+++ contrastive_learning.py
@@ -2,7 +2,7 @@ import torch
 from torchvision import transforms
 from torch.utils.data import DataLoader
 
-from utils import get_datapipe, InfoNCECosine
+from utils import contrastive_datapipe, InfoNCECosine
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -15,36 +15,44 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # magic numbers
 NUM_TRAIN = 115951
 NUM_TEST = 63676
+batch_size = 512
+n_epochs = 200
 
 model_name = "resnet18"
 
 train_transform = transforms.Compose(
     [
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
+        transforms.RandomResizedCrop(
+            size=224,
+            scale=(0.08, 1.0),
+            ratio=(1.0, 1.0),
+        ),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]
 )
 
-datapipe_1 = get_datapipe(
+datapipe_1 = contrastive_datapipe(
     "/mimer/NOBACKUP/groups/naiss2023-5-75/WHOI_Planktons/2013.zip",
     num_images=NUM_TRAIN,
     transforms=train_transform,
     ignore_mix=True,
-    padding=False,
 )
-datapipe_2 = get_datapipe(
+datapipe_2 = contrastive_datapipe(
     "/mimer/NOBACKUP/groups/naiss2023-5-75/WHOI_Planktons/2014.zip",
     num_images=NUM_TEST,
     transforms=train_transform,
     ignore_mix=True,
-    padding=False,
 )
 # combine the two datapipe
 datapipe = datapipe_1.concat(datapipe_2)
 
 # create a dataloader
-train_dataloader = DataLoader(datapipe, batch_size=512, shuffle=True, num_workers=12)
+
+train_dataloader = DataLoader(
+    datapipe, batch_size=batch_size, shuffle=True, num_workers=12
+)
 
 if model_name == "resnet18":
     model = torch.hub.load("pytorch/vision:v0.9.0", "resnet18", pretrained=True)
@@ -59,4 +67,53 @@ projection_head = torch.nn.Sequential(
     torch.nn.Linear(1024, 128),
 )
 
-print(model)
+# combine the model and the projection head
+model = torch.nn.Sequential(model, projection_head)
+
+# optimizer
+max_lr = 0.03 * batch_size / 256
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
+
+# lr scheduler with linear warmup and cosine decay
+scheduler = torch.optim.lr_scheduler.OneCycleLR(
+    optimizer,
+    max_lr=max_lr,
+    epochs=n_epochs,
+    steps_per_epoch=len(train_dataloader),
+    pct_start=0.05,
+    div_factor=1e4,
+    final_div_factor=1e4,
+)
+
+# loss
+criterion = InfoNCECosine(temperature=0.5)
+
+# param count
+print(f"Model: {model_name}")
+print(f"Total parameters: {sum(p.numel() for p in model.parameters())}")
+print(
+    f"Trainable parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)}"
+)
+
+# train the model
+model.train().to(device)
+for epoch in range(n_epochs):
+    for img1, img2, _ in train_dataloader:
+        img1, img2 = img1.to(device), img2.to(device)
+        optimizer.zero_grad()
+        img = torch.cat((img1, img2), dim=0)
+        output = model(img)
+        loss = criterion(output)
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+
+    print(f"Epoch {epoch+1}/{n_epochs}, Loss: {loss.item()}")
+
+# remove the projection head
+model = model[0]
+projection_head = model[1]
+
+# save the model
+torch.save(model.state_dict(), f"finetune_{model_name}.pth")
+torch.save(projection_head.state_dict(), f"finetune_{model_name}_ph.pth")
--- utils.py
+++ utils.py
@@ -105,6 +105,61 @@ def get_datapipe(path, num_images, transforms, ignore_mix=True, padding=False):
     return datapipe
 
 
+def contrastive_datapipe(path, num_images, transforms, ignore_mix=True):
+    dataset_path = path
+    fileopener = FileOpener([dataset_path], mode="b")
+    datapipe = fileopener.load_from_zip()
+
+    def image_filter(data):
+        file_name, _ = data
+        if ignore_mix:
+            return fnmatch(file_name, "*.png") and file_name.split("/")[-2] != "mix"
+        else:
+            return fnmatch(file_name, "*.png")
+
+    datapipe = datapipe.filter(image_filter)
+    datapipe = datapipe.shuffle()
+    datapipe = datapipe.sharding_filter()
+
+    # get label dictionary
+    with open("labels.json") as f:
+        label2id = json.load(f)
+
+    def parse_data(data):
+        file_name, file_content = data
+        id = label2id[file_name.split("/")[-2]]
+        img_array = np.array(Image.open(file_content))
+        if img_array.ndim < 3:
+            img_array = np.repeat(img_array[..., np.newaxis], 3, -1)
+
+        img_tensor = torch.from_numpy(img_array).float()
+        img_tensor = img_tensor.permute(2, 0, 1)
+        img_tensor = img_tensor.div(255)
+        img_tensor_1 = transforms(img_tensor)
+        img_tensor_2 = transforms(img_tensor)
+        return img_tensor_1, img_tensor_2, id
+
+    datapipe = datapipe.map(parse_data)
+    if not hasattr(datapipe, "set_length"):
+
+        @functional_datapipe("set_length")
+        class LengthSetterIterDataPipe(IterDataPipe):
+            def __init__(self, source_datapipe: IterDataPipe, length: int) -> None:
+                self.source_datapipe = source_datapipe
+                assert length >= 0
+                self.length = length
+
+            def __iter__(self) -> IterDataPipe:
+                yield from self.source_datapipe
+
+            def __len__(self) -> int:
+                return self.length
+
+    datapipe = datapipe.set_length(num_images)
+
+    return datapipe
+
+
 class InfoNCECosine(torch.nn.Module):
     def __init__(self, temperature=0.07):
         super(InfoNCECosine, self).__init__()
