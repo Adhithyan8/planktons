Bug fix on F1 score calculation and train/test split varying

contrastive_inference.py
@@ -46,9 +46,16 @@ inference_transform = A.Compose(
     ]
 )
 
-datapipe = inference_datapipe(
+datapipe_train = inference_datapipe(
     [
         "/mimer/NOBACKUP/groups/naiss2023-5-75/WHOI_Planktons/2013.zip",
+    ],
+    num_images=NUM_TOTAL,
+    transforms=inference_transform,
+    padding=padding,
+)
+datapipe_test = inference_datapipe(
+    [
         "/mimer/NOBACKUP/groups/naiss2023-5-75/WHOI_Planktons/2014.zip",
     ],
     num_images=NUM_TOTAL,
@@ -56,7 +63,12 @@ datapipe = inference_datapipe(
     padding=padding,
 )
 
-dataloader = DataLoader(datapipe, batch_size=512, shuffle=False, num_workers=8)
+dataloader_train = DataLoader(
+    datapipe_train, batch_size=512, shuffle=False, num_workers=8
+)
+dataloader_test = DataLoader(
+    datapipe_test, batch_size=512, shuffle=False, num_workers=8
+)
 
 # load model
 backbone = torch.hub.load("pytorch/vision:v0.9.0", "resnet18", pretrained=True)
@@ -68,8 +80,8 @@ projection_head = torch.nn.Sequential(
 )
 
 # load state dict
-backbone.load_state_dict(torch.load(f"{config}_resnet18_backbone.pth"))
-projection_head.load_state_dict(torch.load(f"{config}_resnet18_head.pth"))
+backbone.load_state_dict(torch.load(f"model_weights/{config}_resnet18_backbone.pth"))
+projection_head.load_state_dict(torch.load(f"model_weights/{config}_resnet18_head.pth"))
 
 if head:
     model = torch.nn.Sequential(backbone, projection_head)
@@ -85,7 +97,13 @@ else:
 labels = torch.empty((0,))
 
 model.to(device)
-for images, labels_batch in dataloader:
+for images, labels_batch in dataloader_train:
+    images = images.to(device)
+    with torch.no_grad():
+        output_batch = model(images)
+    output = torch.cat((output, output_batch.cpu()))
+    labels = torch.cat((labels, labels_batch))
+for images, labels_batch in dataloader_test:
     images = images.to(device)
     with torch.no_grad():
         output_batch = model(images)
@@ -98,4 +116,4 @@ labels = labels.numpy()
 
 # save
 np.save(f"embeddings/output_{config}_resnet18{'_head' if head else ''}.npy", output)
-np.save(f"embeddings/labels.npy", labels)
+np.save(f"embeddings/labels_{config}_resnet18{'_head' if head else ''}.npy", labels)

contrastive_learning.py
@@ -1,8 +1,16 @@
 import albumentations as A
 import torch
 from torch.utils.data import DataLoader
+from torchvision.transforms import (
+    AugMix,
+    AutoAugment,
+    Compose,
+    RandAugment,
+    Resize,
+    TrivialAugmentWide,
+)
 
-from utils import InfoNCECosine, Padding, contrastive_datapipe, std_of_l2_normalized
+from utils import InfoNCECosine, Padding, contrastive_datapipe
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -123,10 +131,8 @@ for epoch in range(n_epochs):
         scheduler.step()
 
         if i % 100 == 0:
-            print(
-                f"Epoch [{epoch+1}/{n_epochs}], Loss: {loss.item():.4f}"
-            )
+            print(f"Epoch [{epoch+1}/{n_epochs}], Loss: {loss.item():.4f}")
 
 # save the model
-torch.save(model[0].state_dict(), f"finetune_{model_name}_backbone.pth")
-torch.save(model[1].state_dict(), f"finetune_{model_name}_head.pth")
+torch.save(model[0].state_dict(), f"ft250_{model_name}_backbone.pth")
+torch.save(model[1].state_dict(), f"ft250_{model_name}_head.pth")

pretrained_inference.py
@@ -5,7 +5,7 @@ import numpy as np
 import torch
 from torch.utils.data import DataLoader
 
-from utils import inference_datapipe, Padding
+from utils import Padding, inference_datapipe
 
 # parser
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
@@ -42,13 +42,20 @@ inference_transform = A.Compose(
         A.ToRGB(),
         A.ToFloat(max_value=255),
         A.Normalize(max_pixel_value=1.0),
-        A.Resize(256, 256),
+        A.Resize(252, 252) if model_name == "vitb14-dinov2" else A.Resize(256, 256),
     ]
 )
 
-datapipe = inference_datapipe(
+datapipe_train = inference_datapipe(
     [
         "/mimer/NOBACKUP/groups/naiss2023-5-75/WHOI_Planktons/2013.zip",
+    ],
+    num_images=NUM_TOTAL,
+    transforms=inference_transform,
+    padding=padding,
+)
+datapipe_test = inference_datapipe(
+    [
         "/mimer/NOBACKUP/groups/naiss2023-5-75/WHOI_Planktons/2014.zip",
     ],
     num_images=NUM_TOTAL,
@@ -56,7 +63,12 @@ datapipe = inference_datapipe(
     padding=padding,
 )
 
-dataloader = DataLoader(datapipe, batch_size=512, shuffle=False, num_workers=12)
+dataloader_train = DataLoader(
+    datapipe_train, batch_size=512, shuffle=False, num_workers=12
+)
+dataloader_test = DataLoader(
+    datapipe_test, batch_size=512, shuffle=False, num_workers=12
+)
 
 
 # model (pick one)
@@ -82,7 +94,13 @@ elif model_name == "vitb14-dinov2":
 labels = torch.empty((0,))
 
 model.to(device)
-for images, labels_batch in dataloader:
+for images, labels_batch in dataloader_train:
+    images = images.to(device)
+    with torch.no_grad():
+        output_batch = model(images)
+    output = torch.cat((output, output_batch.cpu()))
+    labels = torch.cat((labels, labels_batch))
+for images, labels_batch in dataloader_test:
     images = images.to(device)
     with torch.no_grad():
         output_batch = model(images)
@@ -95,4 +113,4 @@ labels = labels.numpy()
 
 # save
 np.save(f"embeddings/output_{model_name}.npy", output)
-np.save(f"embeddings/labels.npy", labels)
+np.save(f"embeddings/labels_{model_name}.npy", labels)

tsne_knn.py
@@ -21,7 +21,7 @@ args = vars(parser.parse_args())
 model_name = args["model"]
 visualize = args["visualize"]
 output = np.load(f"embeddings/output_{model_name}.npy")
-labels = np.load("embeddings/labels.npy")
+labels = np.load(f"embeddings/labels_{model_name}.npy")
 
 """
 2013: 421238
@@ -48,19 +48,17 @@ if visualize:
         initialization=init,
     )
 
-    # plot the embedding (only train set)
-    embed_train = embedding[:NUM_TRAIN]
+    # plot the embedding
     plt.figure(figsize=(10, 10))
     plt.scatter(
-        embed_train[:, 0],
-        embed_train[:, 1],
-        c=labels[:NUM_TRAIN],
-        cmap="Spectral",
-        s=0.1,
+        embedding[:, 0], embedding[:, 1], c=labels, cmap="Spectral", s=0.1, alpha=0.8
     )
     plt.axis("off")
-    plt.savefig(f"figures/tsne_train_{model_name}.png", dpi=300)
+    plt.savefig(f"figures/tsne_{model_name}.png", dpi=600)
     plt.close()
+
+    # save embedding
+    np.save(f"embeddings/embeds_{model_name}.npy", embedding)
 
 output_train = output[:NUM_TRAIN]
 output_test = output[NUM_TRAIN:]
@@ -71,9 +69,14 @@ knn.fit(output_train, labels[:NUM_TRAIN])
 
 # test
 preds = knn.predict(output_test)
-f1 = f1_score(labels[NUM_TRAIN:], preds, average="macro")
+f1 = f1_score(labels[NUM_TRAIN:], preds, labels=list(range(103)), average="macro")
 acc = accuracy_score(labels[NUM_TRAIN:], preds)
 
 print(f"{model_name}")
 print(f"Accuracy: {acc}")
 print(f"F1 score: {f1}")
+
+# class wise macro f1 - labels range from 0 to 102
+f1 = f1_score(labels[NUM_TRAIN:], preds, labels=list(range(103)), average=None)
+for i in range(103):
+    print(f"Class {i}: {f1[i]}")

utils.py
@@ -8,6 +8,7 @@ import torch
 from PIL import Image
 from torchdata.datapipes import functional_datapipe
 from torchdata.datapipes.iter import FileOpener, IterDataPipe
+from torchvision.transforms import Normalize
 
 
 class Padding(Enum):
@@ -106,7 +107,10 @@ def contrastive_datapipe(paths, num_images, transforms, padding, ignore_mix=True
     def parse_data(data):
         file_name, file_content = data
         id = label2id[file_name.split("/")[-2]]
-        img_array = np.array(Image.open(file_content))
+        if isinstance(transforms, A.core.composition.Compose):
+            img_array = np.array(Image.open(file_content))
+        else:
+            img_array = np.array(Image.open(file_content).convert("RGB"))
         if padding == Padding.CONSTANT:
             img_array = A.PadIfNeeded(
                 img_array.shape[1], img_array.shape[0], border_mode=0, value=200
@@ -115,10 +119,21 @@ def contrastive_datapipe(paths, num_images, transforms, padding, ignore_mix=True
             img_array = A.PadIfNeeded(
                 img_array.shape[1], img_array.shape[0], border_mode=4
             )(image=img_array)["image"]
-        img_array1 = transforms(image=img_array)["image"]
-        img_array2 = transforms(image=img_array)["image"]
-        img_tensor1 = torch.from_numpy(img_array1).permute(2, 0, 1)
-        img_tensor2 = torch.from_numpy(img_array2).permute(2, 0, 1)
+        if isinstance(transforms, A.core.composition.Compose):
+            img_array1 = transforms(image=img_array)["image"]
+            img_array2 = transforms(image=img_array)["image"]
+            img_tensor1 = torch.from_numpy(img_array1).permute(2, 0, 1)
+            img_tensor2 = torch.from_numpy(img_array2).permute(2, 0, 1)
+        else:
+            img_tensor = torch.from_numpy(img_array).permute(2, 0, 1)
+            img_tensor1 = transforms(img_tensor).div(255)
+            img_tensor2 = transforms(img_tensor).div(255)
+            img_tensor1 = Normalize(
+                mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)
+            )(img_tensor1)
+            img_tensor2 = Normalize(
+                mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)
+            )(img_tensor2)
         return img_tensor1, img_tensor2, id
 
     datapipe = datapipe.map(parse_data)
@@ -178,71 +193,3 @@ def std_of_l2_normalized(z):
     """
     z_norm = torch.nn.functional.normalize(z, dim=1)
     return torch.std(z_norm, dim=0).mean()
-
-
-
-class SearchDataset(torch.utils.data.Dataset):
-
-    def __init__(self, path, num_images, padding, ignore_mix=True, transform=None):
-        self.transform = transform
-        self.path = path
-        self.num_images = num_images
-        self.padding = padding
-        self.ignore_mix = ignore_mix
-        self.transform = transform
-        self.label2id = self.load_label_dict()
-        self.datapipe = self.process_datapipe()
-
-    def load_label_dict(self):
-        with open("/cephyr/users/adhkal/Alvis/planktons/labels.json") as f:
-            return json.load(f)
-
-    def process_datapipe(self):
-        datapipe = FileOpener(self.path, mode="b").load_from_zip()
-
-        def image_filter(data):
-            file_name, _ = data
-            if self.ignore_mix:
-                return fnmatch(file_name, "*.png") and file_name.split("/")[-2] != "mix"
-            else:
-                return fnmatch(file_name, "*.png")
-
-        datapipe = datapipe.filter(image_filter)
-        datapipe = datapipe.shuffle()
-        datapipe = datapipe.sharding_filter()
-        return datapipe
-
-    def parse_data(self, data):
-        file_name, file_content = data
-        label = self.label2id[file_name.split("/")[-2]]
-        img_array = np.array(Image.open(file_content))
-        if self.padding == Padding.CONSTANT:
-            img_array = A.PadIfNeeded(
-                img_array.shape[1], img_array.shape[0], border_mode=0, value=200
-            )(image=img_array)["image"]
-        elif self.padding == Padding.REFLECT:
-            img_array = A.PadIfNeeded(
-                img_array.shape[1], img_array.shape[0], border_mode=4
-            )(image=img_array)["image"]
-        return img_array, label
-
-    def __len__(self):
-        return self.num_images
-
-    def __getitem__(self, index):
-        # Implement logic to get an image and its label using the received index.
-        #
-        # `image` should be a NumPy array with the shape [height, width, num_channels].
-        # If an image contains three color channels, it should use an RGB color scheme.
-        #
-        # `label` should be an integer in the range [0, model.num_classes - 1] where `model.num_classes`
-        # is a value set in the `search.yaml` file.
-
-        data = next(iter(self.datapipe))
-        image, label = self.parse_data(data)
-
-        if self.transform is not None:
-            transformed = self.transform(image=image)
-            image = transformed["image"]
-
-        return image, label