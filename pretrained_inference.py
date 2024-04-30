8af6ee6da158f83785a460931491156b993ef614
Adhithyan Kalaivanan <k.adhithyan@gmail.com>
2024-04-30 14:58:27 +0200

    updated pretrained inference

--- pretrained_inference.py
+++ pretrained_inference.py
@@ -7,25 +7,24 @@ from torch.utils.data import DataLoader
 
 from utils import Padding, inference_datapipe
 
-# parser
+# parse arguments
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-parser.add_argument("-m", "--model", default="resnet18", help="Model architecture")
-parser.add_argument(
-    "-p", "--pad", default="reflect", help="Image padding during inference"
-)
+parser.add_argument("--name", default="resnet18")
+parser.add_argument("--padding", default="reflect")
 args = vars(parser.parse_args())
 
-
-model_name = args["model"]
-if args["pad"] == "constant":
+name = args["name"]
+if args["padding"] == "constant":
     padding = Padding.CONSTANT
-elif args["pad"] == "reflect":
+elif args["padding"] == "reflect":
     padding = Padding.REFLECT
 else:
     padding = None
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 """
+Dataset sizes:
 2013: 421238
 2013: 115951 (ignore mix)
 2014: 329832
@@ -36,13 +35,13 @@ NUM_TRAIN = 115951
 NUM_TEST = 63676
 NUM_TOTAL = NUM_TRAIN + NUM_TEST
 
-
+# transforms and dataloaders
 inference_transform = A.Compose(
     [
         A.ToRGB(),
         A.ToFloat(max_value=255),
         A.Normalize(max_pixel_value=1.0),
-        A.Resize(252, 252) if model_name == "vitb14-dinov2" else A.Resize(256, 256),
+        A.Resize(252, 252) if name == "vitb14-dinov2" else A.Resize(256, 256),
     ]
 )
 
@@ -50,7 +49,7 @@ datapipe_train = inference_datapipe(
     [
         "/mimer/NOBACKUP/groups/naiss2023-5-75/WHOI_Planktons/2013.zip",
     ],
-    num_images=NUM_TOTAL,
+    num_images=NUM_TRAIN,
     transforms=inference_transform,
     padding=padding,
 )
@@ -58,7 +57,7 @@ datapipe_test = inference_datapipe(
     [
         "/mimer/NOBACKUP/groups/naiss2023-5-75/WHOI_Planktons/2014.zip",
     ],
-    num_images=NUM_TOTAL,
+    num_images=NUM_TEST,
     transforms=inference_transform,
     padding=padding,
 )
@@ -70,29 +69,29 @@ dataloader_test = DataLoader(
     datapipe_test, batch_size=512, shuffle=False, num_workers=12
 )
 
-
-# model (pick one)
-if model_name == "resnet18":
+# create model
+if name == "resnet18":
     model = torch.hub.load("pytorch/vision:v0.10.0", "resnet18", pretrained=True)
     model.fc = torch.nn.Identity()
-elif model_name == "resnet50":
+elif name == "resnet50":
     model = torch.hub.load("pytorch/vision:v0.10.0", "resnet50", pretrained=True)
     model.fc = torch.nn.Identity()
-elif model_name == "vitb14-dinov2":
+elif name == "vitb14-dinov2":
     model = torch.hub.load("facebookresearch/dinov2", "dinov2_vitb14_reg")
 else:
     raise ValueError("Invalid model name")
 model.eval()
 
-# store output
-if model_name == "resnet18":
+# storing output
+if name == "resnet18":
     output = torch.empty((0, 512))
-elif model_name == "resnet50":
+elif name == "resnet50":
     output = torch.empty((0, 2048))
-elif model_name == "vitb14-dinov2":
+elif name == "vitb14-dinov2":
     output = torch.empty((0, 768))
 labels = torch.empty((0,))
 
+# inference
 model.to(device)
 for images, labels_batch in dataloader_train:
     images = images.to(device)
@@ -112,5 +111,5 @@ output = output.numpy()
 labels = labels.numpy()
 
 # save
-np.save(f"embeddings/output_{model_name}.npy", output)
-np.save(f"embeddings/labels_{model_name}.npy", labels)
+np.save(f"embeddings/output_{name}.npy", output)
+np.save(f"embeddings/labels_{name}.npy", labels)
